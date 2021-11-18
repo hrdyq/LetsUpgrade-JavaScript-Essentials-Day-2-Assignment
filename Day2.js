69052ea7618b5f2d36a3f3baa5c12a30d21bc2ca
HARDIK PATEL <79811743+hrdyq@users.noreply.github.com>
2021-11-19 02:43:08 +0530

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,18 @@
+a=10
+b=40
+c=30
+
+if(a>b && a>c)
+{
+  console.log(a,"is the biggest number.")
+}
+
+else if (b>a && b>c)
+{
+  console.log(b,"is the biggest number.")
+}
+else
+{
+    console.log(c,"is the biggest number.")
+
+}
